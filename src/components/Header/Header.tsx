007103f816e4916d96ded0fe77d8cbece174aa46
t2techno <ttuschhoff11@gmail.com>
2024-03-22 22:44:20 -0500

    using themeprovider and adding hidden color change button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import styled from "styled-components";
 import BaseBorder from "../FancyBorder";
+import { ThemeContext } from "../../Providers/ThemeProvider";
 
 const Header: React.FC<{ className?: string }> = ({ className }) => {
   console.info("Header ReRender");
+  const { color, setColor } = useContext(ThemeContext);
+  const [showColor, setShowColor] = useState(false);
 
   return (
     <Wrapper className={className}>
-      <Title>Delivery Time!</Title>
+      <ColorChangeButton
+        onClick={() => {
+          setShowColor((color) => !color);
+        }}
+      >
+        <Title>Delivery Time!</Title>
+      </ColorChangeButton>
+      {showColor && (
+        <input
+          type="number"
+          value={color}
+          onChange={(event) =>
+            setColor(Number.parseInt(event.target.value) % 360)
+          }
+        />
+      )}
       <FancyBorder color="var(--dark-mode-background)" />
     </Wrapper>
   );
@@ -23,6 +41,14 @@ const Title = styled.h1`
 
 const FancyBorder = styled(BaseBorder)`
   margin-top: -10%;
+`;
+
+const ColorChangeButton = styled.button`
+  display: block;
+  margin: auto;
+  border: none;
+  color: var(--dark-mode-text-color);
+  background-color: transparent;
 `;
 
 export default Header;
