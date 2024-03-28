c0a6eecec47f11648a8c49bb2163b75f09779494
Tyler Tuschhoff <ttuschhoff11@gmail.com>
2024-03-27 23:46:11 -0500

    Creating contraction provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import ThemeProvider from "./providers/ThemeProvider.tsx";
 import "./reset.css";
+import ContractionProvider from "./providers/ContractionProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <ContractionProvider>
+        <App />
+      </ContractionProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/providers/ContractionProvider.tsx
@@ -0,0 +1,60 @@
+import { PropsWithChildren, createContext, useCallback, useMemo, useState } from "react";
+
+type ContractionOut = {
+    hasStarted: boolean;
+    isContracting: boolean;
+    toggleContractions: (time:number)=>void;
+    numContractions: number;
+    contractionStartTime: number;
+}
+
+interface ContractionItem  {
+    startTime: number;
+    endTime: number;
+
+}
+
+const INIT_CONTEXT_VAL: ContractionOut = {
+    hasStarted: false,
+    isContracting: false,
+    toggleContractions: ()=>console.log('empty Contraction toggle'),
+    numContractions: 0,
+    contractionStartTime: -1
+}
+
+const ContractionContext = createContext<ContractionOut>(INIT_CONTEXT_VAL);
+
+const ContractionProvider: React.FC<PropsWithChildren> = ({children}) => {
+    const [hasStarted, setHasStarted] = useState(false);
+    const [isContracting, setIsContracting] = useState(false);
+    const [contractionStartTime, setContractionStartTime] = useState(-1);
+    const [numContractions, setNumContractions] = useState(0);
+    const [contractionHistory, setContractionHistory] = useState<ContractionItem[]>([]);
+
+    const toggleContractions = useCallback((time: number)=>{
+        setHasStarted(true);
+        setIsContracting(val => {
+            // starting new contraction
+            if(!val){
+                setNumContractions(num => num+1);
+                setContractionStartTime(time);
+            } else {
+                setContractionHistory(history => [...history, {startTime: contractionStartTime, endTime: time}])
+                setContractionStartTime(-1);
+            }
+            return !val
+        });
+    },[])
+
+    const valueOut = useMemo(() => {
+        return ({hasStarted, isContracting, toggleContractions, numContractions, contractionStartTime});
+    },[hasStarted, isContracting, numContractions, contractionStartTime]);
+
+    return (
+        <ContractionContext.Provider value={valueOut}>
+            {children}
+        </ContractionContext.Provider>
+    );
+}
+
+export default ContractionProvider;
