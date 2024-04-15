203e5feef1f72836b1787eb55e455021cf3b77da
Tyler Tuschhoff <ttuschhoff11@gmail.com>
2024-04-14 22:16:31 -0500

    history overhaul

--- src/providers/HistoryProvider.tsx
+++ src/providers/HistoryProvider.tsx
@@ -3,45 +3,93 @@ import React, {
   createContext,
   useCallback,
   useEffect,
+  useMemo,
   useState,
 } from "react";
 import ContractionProvider from "./ContractionProvider";
 import ReminderProvider from "./ReminderProvider";
 
+type HistoryType = 'drink' | 'food' | 'toilet' | 'contraction_start' | 'contraction_stop';
 interface HistoryItem {
-  label: string;
+  id: number;
+  label: HistoryType;
   time: number;
+  contraction: number;
+  timeStamp: string;
+  note: string;
 }
 
+interface ReminderItem {
+  timeStamp: string;
+  elapsedTime: number;
+  contraction: number;
+}
+export const EMPTY_REMINDER: ReminderItem = {
+  timeStamp: "",
+  elapsedTime: 0,
+  contraction: 0,
+};
+
 interface ValueOut {
-  addHistoryItem: (label: string, time: number) => void;
+  addHistoryItem: (label: HistoryType, time: number, contraction: number) => number;
+  lastDrink: ReminderItem;
+  lastFood: ReminderItem;
+  lastToilet: ReminderItem;
 }
 const INIT_VALUE: ValueOut = {
   addHistoryItem: () => {
     console.log("Empty History add method");
+    return 0;
   },
+  lastDrink: EMPTY_REMINDER,
+  lastFood: EMPTY_REMINDER,
+  lastToilet: EMPTY_REMINDER
 };
-export const HistoryContext = createContext<ValueOut>(INIT_VALUE);
 
+export const HistoryContext = createContext<ValueOut>(INIT_VALUE);
 const HistoryProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const KEY = "history";
-  const [history, setHistory] = useState<HistoryItem[]>(() => {
+  const [history, setHistory] = useState<Array<HistoryItem>>(() => {
     const savedHistory = window.localStorage.getItem(KEY);
     return savedHistory != null ? JSON.parse(savedHistory) : [];
   });
 
+  // list of relevant ids
+  const [drinkLog, setDrinkLog] = useState<Array<number>>([]);
+  const [lastDrink, setLastDrink] = useState<ReminderItem>(EMPTY_REMINDER);
+  const [foodLog, setFoodLog] = useState<Array<number>>([]);
+  const [lastFood, setLastFood] = useState<ReminderItem>(EMPTY_REMINDER);
+  const [toiletLog, setToiletLog] = useState<Array<number>>([]);
+  const [lastToilet, setLastToilet] = useState<ReminderItem>(EMPTY_REMINDER);
   // save history to local storage
   useEffect(() => {
     window.localStorage.setItem(KEY, JSON.stringify(history));
   }, [history]);
 
-  const addHistoryItem = useCallback((label: string, time: number) => {
-    console.log(`${label} - ${time} - ${new Date().toLocaleString()}`);
-    setHistory((h) => [...h, { label, time }]);
+  const addHistoryItem = useCallback((label: HistoryType, time: number, contraction: number): number => {
+    const id = Math.random();
+    const newHistoryItem: HistoryItem = {
+      id,
+      label,
+      time,
+      contraction,
+      timeStamp: new Date().toLocaleString(),
+      note: ""
+    }
+    switch(label) {
+      
+    }
+
+    setHistory((h) => [...h, newHistoryItem]);
+    return id;
   }, []);
 
+  const value: ValueOut = useMemo(()=>{
+    return {addHistoryItem, lastDrink, lastFood, lastToilet}
+  },[lastDrink, lastFood, lastToilet]);
+
   return (
-    <HistoryContext.Provider value={{ addHistoryItem }}>
+    <HistoryContext.Provider value={value}>
       <ContractionProvider>
         <ReminderProvider>{children}</ReminderProvider>
       </ContractionProvider>
